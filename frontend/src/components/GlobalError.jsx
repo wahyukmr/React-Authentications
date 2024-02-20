05e18c380ce012f6c5623a9fbfe25673798b1d5d
wahyukmr <maryu9229@gmail.com>
2024-02-20 12:06:31 +0700

    [update] 🚀 updating the global-error

--- frontend/src/components/GlobalError.jsx
+++ frontend/src/components/GlobalError.jsx
@@ -1,22 +1,27 @@
 /* eslint-disable react/prop-types */
-import {
-  isRouteErrorResponse,
-  useNavigate,
-  useRouteError,
-} from "react-router-dom";
+import { ShieldExclamationIcon } from "@heroicons/react/20/solid";
+import { isRouteErrorResponse, useNavigate, useRouteError } from "react-router-dom";
+import styles from "./GlobalError.module.css";
 
 export default function GlobalError() {
   const navigate = useNavigate();
   const errorInfo = useRouteError();
 
-  if (!isRouteErrorResponse(errorInfo)) null;
+  if (!isRouteErrorResponse(errorInfo)) return null;
 
   return (
-    <div className="global-error">
-      <h1>{errorInfo.status}</h1>
+    <div className={styles["global-error"]}>
+      <ShieldExclamationIcon className={styles["global-error__icon"]} />
+      <h1 className={styles["global-error__h1"]}>{errorInfo.status}</h1>
       <h2>{errorInfo.statusText}</h2>
       <p>{errorInfo.error?.message ?? "Something went wrong!!"}</p>
-      <button onClick={() => navigate("/")}>Back to Home Page</button>
+      <button
+        className={styles["global-error__button"]}
+        type="button"
+        onClick={() => navigate("/")}
+      >
+        Back to Home Page
+      </button>
     </div>
   );
 }
